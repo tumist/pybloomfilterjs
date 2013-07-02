dedbfffdaa5d470f059e12c5670af532257805df
Tumi Steingrímsson <tumi@mbl.is>
2013-07-02 17:49:32 +0000

    Binary and octet stream encodings

--- bloomfilterjs.py
+++ bloomfilterjs.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import struct
 from ctypes import c_uint32, c_int32
 
 FNV_prime = 0x01000193
@@ -46,7 +47,7 @@ class BloomFilter(object):
     def __init__(self, m, k):
         self.m = m
         self.k = k
-        n = m/32 + (1 if m%32 else 0)
+        n = m/32 + (1 if m%32 else 0) # 32 bits per bucket
         self.buckets = [0] * n
         self._locations = [0] * k
 
@@ -61,11 +62,13 @@ class BloomFilter(object):
         return r
 
     def add(self, value):
+        """Add value"""
         l = self.locations(value)
         for i in range(self.k):
             self.buckets[l[i]/32] |= 1 << (l[i] % 32)
 
     def test(self, value):
+        """Test value membership"""
         l = self.locations(value)
         for i in range(self.k):
             b = l[i]
@@ -73,12 +76,27 @@ class BloomFilter(object):
                 return False
         return True
 
+    def encode_base2(self):
+        binbuckets = []
+        for b in self.buckets:
+            binbuckets.append(''.join(str((b>>i)&1) for i in range(31)))
+        return ''.join(binbuckets)[:self.m]
+
+    def encode_octets(self):
+        octbuckets = []
+        for b in self.buckets:
+            octbuckets.append(struct.pack('I', b))
+        return ''.join(octbuckets)[:self.m/8 + (1 if self.m%8 else 0)]
+
 
 if __name__ == "__main__":
     bloom = BloomFilter(32, 16)
     bloom.add("foo")
     bloom.add("bar")
 
-    print bloom.test("foo")
-    print bloom.test("bar")
-    print bloom.test("blah")
+    print "test foo:", bloom.test("foo")
+    print "test bar:", bloom.test("bar")
+    print "test blah:", bloom.test("blah")
+    print "bucket contents:", bloom.buckets
+    print "base2 encoding:", bloom.encode_base2()
+    print "octet encoding:", repr(bloom.encode_octets())
