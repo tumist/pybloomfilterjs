ac38d3e487915e8773ae485beae37ad29dd9a208
Tumi Steingrímsson <tumi.st@gmail.com>
2013-07-02 19:01:14 +0000

    Renamed project to pybloomfilterjs and split source

---
+++ pybloomfilterjs/__init__.py
@@ -0,0 +1,2 @@
+from hash import fnv_1a, fnv_1a_b
+from filter import BloomFilter
--- pybloomfilterjs/filter.py
+++ pybloomfilterjs/filter.py
@@ -1,47 +1,6 @@
 # -*- coding: utf-8 -*-
 import struct
-from ctypes import c_uint32, c_int32
-
-FNV_prime = 0x01000193
-
-def fnv_1a(message):
-    if isinstance(message, str):
-        message = [ord(c) for c in message]
-    else:
-        raise ValueError
-    # XOR and multiplication in javascript implementation
-    # simulates C uint32 behavior.
-    digest = c_uint32(0x811c9dc5)
-    for c in message:
-        digest.value ^= c
-        digest.value *= FNV_prime
-    # The following is not a part of FNV_1A but modifictations suggested for
-    # good avalance behavior and uniform distribution, see:
-    #  http://home.comcast.net/~bretm/hash/6.html
-    # Overflows for this part should behave like javascripts' number type
-    digest = c_int32(digest.value)
-    digest.value += digest.value << 13
-    digest.value ^= digest.value >> 7
-    digest.value += digest.value << 3
-    digest.value ^= digest.value >> 17
-    digest.value += digest.value << 5
-    return digest.value
-
-def fnv_1a_b(digest):
-    if not isinstance(digest, c_int32):
-        digest = c_int32(digest)
-    digest.value += (digest.value << 1) + \
-                    (digest.value << 4) + \
-                    (digest.value << 7) + \
-                    (digest.value << 8) + \
-                    (digest.value << 24);
-    digest.value += digest.value << 13;
-    digest.value ^= digest.value >> 7;
-    digest.value += digest.value << 3;
-    digest.value ^= digest.value >> 17;
-    digest.value += digest.value << 5;
-    return digest.value
-
+from hash import fnv_1a, fnv_1a_b
 
 class BloomFilter(object):
     def __init__(self, m, k):
